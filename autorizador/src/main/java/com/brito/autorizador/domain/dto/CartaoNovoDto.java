5cf981c88eeeaa564443425ca8f406f2a51cd612
Jonathan Brito Costa <devjonathanbrito@gmail.com>
2023-12-12 07:46:02 -0300

    Refatorando os dtos e implementando o records

--- autorizador/src/main/java/com/brito/autorizador/domain/dto/CartaoNovoDto.java
+++ autorizador/src/main/java/com/brito/autorizador/domain/dto/CartaoNovoDto.java
@@ -5,51 +5,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-import java.io.Serializable;
-
-public class CartaoNovoDto implements Serializable {
-
-    private static final long serialVersionUID = -6136282345225004572L;
-
-    @NotNull
-    @NotBlank
-    @Size(min = 16, message = "O cartão deve conter 16 números")
-    @Size(max = 16, message = "O cartão deve conter 16 números")
-    private String numeroCartao;
-
-    @NotNull
-    @NotBlank
-    @Size(min = 4, message = "A senha deve conter 4 números")
-    @Size(max = 4, message = "A senha deve conter 4 números")
-    private String senha;
-
-    public CartaoNovoDto(){
-
-    }
-
-    public CartaoNovoDto(String numeroCartao, String senha) {
-        this.numeroCartao = numeroCartao;
-        this.senha = senha;
-    }
-
-    public String getNumeroCartao() {
-        return numeroCartao;
-    }
-
-    public void setNumeroCartao(String numeroCartao) {
-        this.numeroCartao = numeroCartao;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    @Override
-    public String toString() {
-        return "Cartao [numeroCartao=" + numeroCartao + ", senha=" + senha + "]";
-    }
+public record CartaoNovoDto(
+        @NotNull @NotBlank
+        @Size(min = 16, message = "O cartão deve conter 16 números")
+        @Size(max = 16, message = "O cartão deve conter 16 números")
+        String numeroCartao,
+        @NotNull @NotBlank
+        @Size(min = 4, message = "A senha deve conter 4 números")
+        @Size(max = 4, message = "A senha deve conter 4 números")
+        String senha) {
 }
+
--- autorizador/src/main/java/com/brito/autorizador/domain/dto/TransacaoNovaDto.java
+++ autorizador/src/main/java/com/brito/autorizador/domain/dto/TransacaoNovaDto.java
@@ -5,50 +5,11 @@ import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
-public class TransacaoNovaDto {
-
-    @NotNull
-    @NotBlank
-    private String numeroCartao;
-
-    @NotNull
-    @NotBlank
-    private String senhaCartao;
-
-    @NotNull
-    private BigDecimal valor;
-
-    public TransacaoNovaDto(){
-
-    }
-
-    public TransacaoNovaDto(String numeroCartao, String senhaCartao, BigDecimal valor){
-        this.numeroCartao = numeroCartao;
-        this.senhaCartao = senhaCartao;
-        this.valor = valor;
-    }
-
-    public String getNumeroCartao() {
-        return numeroCartao;
-    }
-
-    public void setNumeroCartao(String numeroCartao) {
-        this.numeroCartao = numeroCartao;
-    }
-
-    public String getSenhaCartao() {
-        return senhaCartao;
-    }
-
-    public void setSenhaCartao(String senhaCartao) {
-        this.senhaCartao = senhaCartao;
-    }
-
-    public BigDecimal getValor() {
-        return valor;
-    }
-
-    public void setValor(BigDecimal valor) {
-        this.valor = valor;
-    }
+public record TransacaoNovaDto(
+        @NotNull @NotBlank
+        String numeroCartao,
+        @NotNull @NotBlank
+        String senhaCartao,
+        @NotNull BigDecimal valor) {
 }
+
