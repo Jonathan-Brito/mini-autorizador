1b353dbffa995c145076febc87003c7319a2b45d
Jonathan Brito Costa <devjonathanbrito@gmail.com>
2023-12-12 08:38:29 -0300

    Refatorando CartaoRepository

--- autorizador/src/main/java/com/brito/autorizador/domain/repositories/CartaoRepository.java
+++ autorizador/src/main/java/com/brito/autorizador/domain/repositories/CartaoRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartaoRepository extends JpaRepository<Cartao, String> {
+public interface CartaoRepository extends JpaRepository<Cartao, Long> {
 
     Cartao findByNumeroCartao(String numeroCartao);
 }
